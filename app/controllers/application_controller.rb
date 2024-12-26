4861c8d980c68f56c6869e522a95334eb3804a9f
uk27711 <ec2-user@ip-172-31-14-99.ap-northeast-1.compute.internal>
2024-12-26 02:09:47 +0000

    20241226

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+def show
+  @list = List.find(params[:id])
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+
+  get 'lists/:id' => 'lists#show'
 end
