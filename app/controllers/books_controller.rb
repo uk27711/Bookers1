990bcec805b6183392a44256f6fd9e6320e6acc9
uk27711 <ec2-user@ip-172-31-14-99.ap-northeast-1.compute.internal>
2025-01-03 15:05:11 +0000

    20250103

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,49 +7,21 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
-
-    if request.post? && params[:book].present?
-      @book = Book.new(book_params)
-      if @book.save
-      flash.now[:notice] = "Book was successfully created."
-      redirect_to book_path(@book)
-      else
-      if @book.errors[:title].any?
-      flash.now[:alert] = "Title can't be blank."
-    elsif @book.errors[:body].any?
-      flash.now[:alert] = "Body can't be blank."
-      end
-    end
-  end
 end
 
   def show
     @book = Book.find(params[:id])
-    if params[:id] == 'edit'
-      redirect_to edit_books_path(id: 'edit')
-    else
-    @book = Book.find(params[:id])
-    end
   end
 
   def edit
     @book = Book.find(params[:id])
-    if params[:id] == 'edit'
-      @book = Book.new
-    else
-      @book = Book.find(params[:id])
-    end
   end
 
   def update
     @book = Book.find(params[:id])
-    if @book.update(book_params)
-      flash[:notice] = 'Book was successfully created.'
-    redirect_to books_path(@book)
-    else
-      @books = Book.all
-      render :index
-    end
+      book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to books_path(book.id)
   end
 
 
@@ -57,7 +29,7 @@ end
     @book = Book.new(book_params)
      if @book.save
       flash[:notice] = 'Book was successfully created.'
-      redirect_to books_path(@book.id)
+      redirect_to books_path(book.id)
      else
       @books = Book.all
       render :index
@@ -66,8 +38,9 @@ end
 
   def destroy
     @book = Book.find(params[:id])
-    redirect_to books_path(@book.id), notice: 'アイテムが削除されました。'
     @book.destroy
+    redirect_to books_path(book.id), notice: 'Book was successfully destroyed.'
+    
   end
 
 private
