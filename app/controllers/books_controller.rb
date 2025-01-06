cf1f5e4180b1b945590d55ceea6bd78fbc4ff856
uk27711 <ec2-user@ip-172-31-14-99.ap-northeast-1.compute.internal>
2025-01-06 14:25:43 +0000

    20250106_Rspec完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -35,7 +35,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
      if @book.save
       flash[:notice] = 'Book was successfully created.'
-      redirect_to books_path(@book.id)
+      redirect_to book_path(@book.id)
      else
       @books = Book.all
       render :index
@@ -45,7 +45,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path(@book.id), notice: 'Book was successfully destroyed.'
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
     
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
   root 'homes#top'
   get '/top', to: 'homes#top'
-  get '/books', to: 'books#index'
-  get 'books/:id/show' => 'books#show', as: 'show_book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  get 'home/top' => 'homes#top'
-  put 'books/:id' =>'books#update'
-  delete 'books/:id' => 'books#destroy'
 
 
 resources :books
-  post 'books' => 'books#create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
